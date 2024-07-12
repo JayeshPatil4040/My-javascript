73caf137d6192748d5b72511275fb0c06bf04036
JayeshPatil4040 <cp809375@gmail.com>
2024-07-12 15:00:38 +0000

    javascript practice throught github

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const UserId = "User1@x-men";
+let Email = "demmy2423@gmail.com";
+var Password = "453423";
+CityName = "mumbai";
+let accountState ;
+
+console.table([UserId , Email , Password , CityName,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; // treat all Js code as newer version
+
+//alert("Hello world") // We are using nodejs , not browser
+
+console.log
+(
+    "Hello world "
+) // we should not write this sentact , code should be readable 
+
