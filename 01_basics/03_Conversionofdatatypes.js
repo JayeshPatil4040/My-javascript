a927122e0b73f1780872bec652a3a885e20fee79
JayeshPatil4040 <cp809375@gmail.com>
2024-07-14 12:04:49 +0000

    Conversionofdatatypes

---
+++ 01_basics/03_Conversionofdatatypes.js
@@ -0,0 +1,26 @@
+let userName = undefined;
+let userAge = 45;
+
+let usertype = Number(userName);
+
+// console.log(typeof userName);
+// console.log(typeof usertype);
+// console.log(usertype)
+
+
+// "99" = 99
+//"99er" = NaN
+//true = 1, false = 0
+//null = 0
+//undefined = NaN
+
+let typeBoolen = undefined;
+let convertboolen = Boolean(typeBoolen);
+
+console.log(convertboolen);
+
+// 1 = true 
+// 0 = false
+// "" = false
+// "rrr" = true
+// undefined = false
