74c4acdf4aac2ad88455ffc7b1a65cdd9cbb9a7f
JayeshPatil4040 <cp809375@gmail.com>
2024-07-20 13:04:13 +0000

    dataTypesummery

---
+++ 01_basics/04_comparistionDatatypes.js
@@ -0,0 +1,15 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0 );
+console.log(null <= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0 );
+console.log(undefined <= 0);
+
+// We should avoide this type of conversion 
+// Because this type of conversion creates confusion
---
+++ 01_basics/05_dataTypesummery.js
@@ -0,0 +1,18 @@
+// Primitive types
+// There are 7 types  of that i known they are
+// Strint , Number , Boolearn , null , undefined , Symbol , BigInt
+
+const id = Symbol(123);
+const anotherId = Symbol(123);
+//Symbol is used to show uniqe number and string
+//exmaple id of student in the school
+
+console.log(id === anotherId);
+
+const BigInt = 3405456666777888669n
+
+console.log(BigInt);
+
+// Reference type (Non primitive)
+
+// Array , Function , Odjects
