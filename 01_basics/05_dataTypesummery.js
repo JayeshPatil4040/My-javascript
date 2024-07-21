1ee5c07d879cf53d5a9a538de39b1f1f2188af79
JayeshPatil4040 <cp809375@gmail.com>
2024-07-21 13:59:44 +0000

    mameryAlocation

--- 01_basics/05_dataTypesummery.js
+++ 01_basics/05_dataTypesummery.js
@@ -1,4 +1,11 @@
 // Primitive types
+// In primitive data type the orignal does not change 
+// E.g
+
+let primitive = "Test message";
+let primitiveTwo = primitive;
+primitiveTwo = "Test message two"
+
 // There are 7 types  of that i known they are
 // Strint , Number , Boolearn , null , undefined , Symbol , BigInt
 
---
+++ 01_basics/06_mameryAlocation.js
@@ -0,0 +1,13 @@
+// There are to types of mamery there are 
+// stack and heap 
+// stack mamery are used in all primitive data type
+//Heap mamery are used in all reference data type
+
+// Example 
+
+let stackMamery = "Test message"; // This value is store in stack mamery.
+
+let heapMamery = {
+    Name : "Adam",
+    Id : 401,
+};  // This values are store in heap mamery.
