1b4a9f6cfed26cb5386998e4dd3568ebe53a64c2
JayeshPatil4040 <cp809375@gmail.com>
2024-07-21 14:46:51 +0000

    object

---
+++ 01_ObjectJavascript/01_Object Literal.js
@@ -0,0 +1,31 @@
+let Password = Symbol("Password")
+let myObj = {
+    Name : "Alex",
+    LastName : "Panden",
+    Age : 23,
+    IsMarried : true,
+    Address : {
+        City : "Mumbai",
+        State : "Maharashtra"
+    },
+    Email : "panden@.com",
+    [Password] : "12345",
+    Phone : "1234567890",
+    PaymentResponse : "Success"
+}
+
+
+console.log(myObj.Address.City)
+console.log(myObj[Password])
+
+ myObj.SayHello = function(){
+    console.log("Hello")
+}
+
+myObj.getName = function(){
+    console.log(`Your name is ${myObj.Name} ${myObj.LastName}`) // alert(myObj.Name)
+    console.log(`My city is ${myObj.Address.City} and state is ${myObj.Address.State}`)
+}
+
+console.log(myObj.getName());
+console.log(myObj.SayHello());
