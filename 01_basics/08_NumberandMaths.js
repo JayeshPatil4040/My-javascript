04b372c3c9696e5875cf049effb58821925404ce
JayeshPatil4040 <cp809375@gmail.com>
2024-08-11 13:57:33 +0000

    Number and maths functions in js

---
+++ 01_basics/08_NumberandMaths.js
@@ -0,0 +1,37 @@
+let getRupess = 100;
+const getDollor = new Number(3000000);
+
+// console.log(getRupess);
+// console.log(getDollor);
+// console.log(getDollor.toFixed(2));
+// console.log(getDollor.toLocaleString('en-in'));
+
+const getNum = 123.5450
+console.log(getNum.toPrecision(3));
+
+
+const getNum2 = 11123.3450
+console.log(getNum2.toPrecision(4));
+
+// ********** Maths ***********
+
+const num1 = Math;
+// Converts nagitive number into positive number
+// console.log(Math.abs(-3));
+// console.log(Math.round(12.9));
+// console.log(Math.ceil(12.9));
+// console.log(Math.floor(12.9));
+// console.log(Math.sqrt(4));
+// console.log(Math.min(4,5,6,8,9));
+// console.log(Math.max(4,5,6,8,9));
+
+// console.log(Math.random());
+// console.log(Math.random() * 10);
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor((Math.random() * 10) + 1));
+// console.log(Math.ceil((Math.random() * 10) + 1));
+
+let minNum = 50;
+let maxNum = 60;
+
+console.log(Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
