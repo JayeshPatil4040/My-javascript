3df02b5dd6a7035eac03a4b89c0746d0d35ecfc1
JayeshPatil4040 <cp809375@gmail.com>
2024-08-22 14:15:25 +0000

    Array_1

---
+++ 01_basics/10_Arrays.js
@@ -0,0 +1,6 @@
+// Javascript arrays are resizeble and they content mix of different data types
+//JavaScript arrays are not associative arrays and so, array elements cannot be accessed using arbitrary strings as indexes.
+//JavaScript arrays are zero-indexed: the first element of an array is at index 0, the second is at index 1.
+//Javascript arrays copy operation creates a SHELLOW COPY rather than DEEP COPY
+// 1) : Shellow copy - Shellow copy means any changes we make in orignal array it will also change orignal array.
+//2)  : Deep copy - Deep copy means any changes we make in orignal array it will not change the orignal array
